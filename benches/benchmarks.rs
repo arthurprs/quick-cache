55bd7a0e70f16051b1ec877e31f89f68446bb861
Arthur Silva <arthurprs@gmail.com>
2022-08-13 20:18:34 +0200

    Adjust benchmark sample count

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,10 +5,11 @@ use rand::rngs::SmallRng;
 use rand_distr::Zipf;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    const N_SAMPLES: usize = 10_000;
     for population in [10_000, 1_000_000] {
         for s in [0.5, 0.75] {
             let mut g = c.benchmark_group(format!("Zipf N={} S={}", population, s));
-            g.throughput(criterion::Throughput::Elements(population));
+            g.throughput(criterion::Throughput::Elements(N_SAMPLES));
             for capacity_ratio in [0.05, 0.1, 0.15] {
                 let capacity = (population as f64 * capacity_ratio) as usize;
                 g.bench_function(format!("qc {}", capacity), |b| {
@@ -19,14 +20,14 @@ pub fn criterion_benchmark(c: &mut Criterion) {
                             let mut rng = SmallRng::seed_from_u64(1);
                             let mut dist = Zipf::new(population, s).unwrap();
                             let mut cache = Cache::new(capacity, capacity);
-                            for i in 0..population * 10 {
+                            for i in 0..population * 3 {
                                 let sample = dist.sample(&mut rng) as usize;
                                 cache.insert(sample, sample);
                             }
                             (rng, dist, cache)
                         },
                         |(rng, dist, cache)| {
-                            for _ in 0..population {
+                            for _ in 0..N_SAMPLES {
                                 let sample = dist.sample(rng) as usize;
                                 if cache.get(&sample).is_some() {
                                     _hits += 1;
