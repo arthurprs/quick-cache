3aa3173252a4666388a2bb27f1e37811d2c574bb
Arthur Silva <arthurprs@gmail.com>
2023-12-11 22:58:55 +0100

    Fix lifecycle after ColdDemoted removal

--- fuzz/fuzz_targets/fuzz_cache.rs
+++ fuzz/fuzz_targets/fuzz_cache.rs
@@ -15,21 +15,22 @@ impl Weighter<u16, (u16, u16)> for MyWeighter {
 }
 
 impl Lifecycle<u16, (u16, u16)> for MyLifecycle {
-    type RequestState = ();
+    type RequestState = Vec<(u16, (u16, u16))>;
 
     fn begin_request(&self) -> Self::RequestState {
-        ()
+        Default::default()
     }
 
     fn before_evict(&self, _state: &mut Self::RequestState, _key: &u16, val: &mut (u16, u16)) {
+        // eprintln!("Before evict {_key} {val:?}");
         if val.0 % 5 == 0 {
             val.1 = 0;
         }
-        // eprintln!("Before evict {_key} {val:?}");
     }
 
-    fn on_evict(&self, _state: &mut Self::RequestState, _key: u16, _val: (u16, u16)) {
-        // eprintln!("Evicted {_key}");
+    fn on_evict(&self, state: &mut Self::RequestState, key: u16, val: (u16, u16)) {
+        // eprintln!("Evicted {key}");
+        state.push((key, val));
     }
 }
 
@@ -56,24 +57,41 @@ fuzz_target!(|ops: Vec<u16>| {
     for (i, op) in ops.iter().enumerate() {
         if i % 8 == 0 {
             // eprintln!("remove {op}");
-            cache.remove(op);
+            if let Some((rem_k, _)) = cache.remove(op) {
+                assert_eq!(rem_k, *op);
+            }
             assert!(cache.get(op).is_none());
         } else if i % 10 == 0 {
             let v = i as u16;
             // eprintln!("replace {op} {v}");
-            if cache.replace(*op, (v, v), false).is_ok() {
+            if let Ok(evicted) = cache.replace_with_lifecycle(*op, (v, v), false) {
+                // if k is present it must have value v
                 let get = cache.get(op);
                 assert!(get.is_none() || get.unwrap().0 == v);
+                check_evicted(*op, get, evicted);
             } else {
                 assert!(cache.get(op).is_none());
             }
         } else {
             let v = i as u16;
             // eprintln!("insert {op} {v}");
-            cache.insert(*op, (v, v));
+            let evicted = cache.insert_with_lifecycle(*op, (v, v));
+            // if k is present it must have value v
             let get = cache.get(op);
             assert!(get.is_none() || get.unwrap().0 == v);
+            check_evicted(*op, get, evicted);
         }
     }
     cache.validate();
 });
+
+fn check_evicted(key: u16, get: Option<(u16, u16)>, evicted: Vec<(u16, (u16, u16))>) {
+    let mut evicted_hm = ahash::HashSet::default();
+    evicted_hm.reserve(evicted.len());
+    for (k, (_, w)) in evicted {
+        // we can't evict a 0 weight item, unless it was for the same key
+        assert!(k == key || w != 0);
+        // we can't evict something twice, except if the insert displaced an old old value but the new value also got evicted
+        assert!(evicted_hm.insert(k) || (k == key && get.is_none()));
+    }
+}
