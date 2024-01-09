c6151d30c97f256b06a88a90124539df257df30c
Arthur Silva <arthurprs@gmail.com>
2024-01-09 16:29:35 -0300

    Update readme, examples and CI

---
+++ examples/custom_weight.rs
@@ -0,0 +1,19 @@
+use quick_cache::{sync::Cache, Weighter};
+
+#[derive(Clone)]
+struct StringWeighter;
+
+impl Weighter<u64, String> for StringWeighter {
+    fn weight(&self, _key: &u64, val: &String) -> u32 {
+        // Be cautions out about zero weights!
+        val.len().clamp(1, u32::MAX as usize) as u32
+    }
+}
+
+fn main() {
+    let cache = Cache::with_weighter(100, 100_000, StringWeighter);
+    cache.insert(1, "1".to_string());
+    cache.insert(54, "54".to_string());
+    cache.insert(1000, "1000".to_string());
+    assert_eq!(cache.get(&1000).unwrap(), "1000");
+}
