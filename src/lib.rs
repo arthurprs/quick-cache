4ff3280c8964f6ef555dc5af95d7a65051934780
Arthur Silva <arthurprs@gmail.com>
2024-05-19 16:05:06 +0200

    Utilize u64 weights

--- src/lib.rs
+++ src/lib.rs
@@ -93,9 +93,9 @@ pub type DefaultHashBuilder = std::collections::hash_map::RandomState;
 /// struct StringWeighter;
 ///
 /// impl Weighter<u64, String> for StringWeighter {
-///     fn weight(&self, _key: &u64, val: &String) -> u32 {
+///     fn weight(&self, _key: &u64, val: &String) -> u64 {
 ///         // Be cautions out about zero weights!
-///         val.len().clamp(1, u32::MAX as usize) as u32
+///         val.len() as u64
 ///     }
 /// }
 ///
@@ -114,7 +114,10 @@ pub trait Weighter<Key, Val> {
     ///
     /// Note that this it's undefined behavior for a cache item to change its weight.
     /// The only exception to this is when Lifecycle::before_evict is called.
-    fn weight(&self, key: &Key, val: &Val) -> u32;
+    ///
+    /// It's also undefined behavior in release mode if summing of weights overflow,
+    /// although this is unlikely to be a problem in pratice.
+    fn weight(&self, key: &Key, val: &Val) -> u64;
 }
 
 /// Each cache entry weights exactly `1` unit of weight.
@@ -123,7 +126,7 @@ pub struct UnitWeighter;
 
 impl<Key, Val> Weighter<Key, Val> for UnitWeighter {
     #[inline]
-    fn weight(&self, _key: &Key, _val: &Val) -> u32 {
+    fn weight(&self, _key: &Key, _val: &Val) -> u64 {
         1
     }
 }
@@ -190,8 +193,8 @@ mod tests {
         struct StringWeighter;
 
         impl Weighter<u64, String> for StringWeighter {
-            fn weight(&self, _key: &u64, val: &String) -> u32 {
-                val.len().clamp(1, u32::MAX as usize) as u32
+            fn weight(&self, _key: &u64, val: &String) -> u64 {
+                val.len() as u64
             }
         }
 
